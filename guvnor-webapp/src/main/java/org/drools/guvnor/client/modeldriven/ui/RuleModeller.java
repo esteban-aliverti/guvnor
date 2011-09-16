0edad4ea12502302fc29020be02647b3b0cf7dc5
esteban-aliverti <esteban.aliverti@gmail.com>
2011-09-16 10:00:51 +0200

    Guided Editor: 	- Rule verification now also ocurrs when adding a new Pattern 	- Rule verification is fired when a Pattern is modified (it is only executed if auto-verification is on)

--- guvnor-webapp/src/main/java/org/drools/guvnor/client/modeldriven/ui/RuleModeller.java
+++ guvnor-webapp/src/main/java/org/drools/guvnor/client/modeldriven/ui/RuleModeller.java
@@ -91,6 +91,7 @@ public class RuleModeller extends DirtyableComposite
 
                                                                   public void execute() {
                                                                       hasModifiedWidgets = true;
+                                                                      verifyRule(null);
                                                                   }
                                                               };
 
